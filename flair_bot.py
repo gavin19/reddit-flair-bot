4d648e261dc778cf9c83dae9cfeb1bab9d74ba4b
Gavin <gavin.19@gmail.com>
2014-11-18 17:13:24 +0000

    Move into a class

--- flair_bot.py
+++ flair_bot.py
@@ -6,7 +6,7 @@ from time import gmtime, strftime
 try:
     from flair_list import flairs
 except ImportError as e:
-    print("Flairs file can't be accessed")
+    print("Flairs file can't be accessed\n")
     print(e)
     sys.exit()
 except SyntaxError as e:
@@ -14,42 +14,72 @@ except SyntaxError as e:
     print(e)
     sys.exit()
 
-UA = '/u/some_user flair bot for /r/some_subreddit'
-USER_NAME = 'user_name'
-PASSWD = 'password'
-# The SUBJECT will be the default subject of your PMs
-# when you create the URLs. PMs require a subject
-# but it's also a simple way of identifying
-# PMs that are directed towards the flairs
-# and not just a general PM
-SUBJECT = 'crest'
-# TARGET_SUB is to be the name of the subreddit
-# without the leading /r/
-TARGET_SUB = 'example_subreddit'
 
-def main():
-    print "Fired: " + strftime("%Y-%m-%d %H:%M:%S", gmtime())
-    r = praw.Reddit(user_agent=UA)
-    r.login(USER_NAME, PASSWD)
-    for msg in r.get_unread(limit=None):
-        subj = str(msg.subject)
-        print "Subject: " + subj
-        if subj == SUBJECT:
-            print msg
-            auth = str(msg.author)
-            body = str(msg.body)
-            print "Author: " + auth
-            print "Message content: " + body
-            sub = r.get_subreddit(TARGET_SUB)
-            if body in flairs:
-                ftext = str(flairs[body])
-                sub.set_flair(auth, ftext, body)
-                with open('log.txt', 'a') as logfile:
-                    tn = strftime("%Y-%m-%d %H:%M:%S", gmtime())
-                    lm = ' : ' + body + ' @ ' + tn
-                    logfile.write('\n\rAdded: ' + auth + ' : ' + ftext + lm)
-                print "Setting flair: " + auth + " : " + ftext + " : " + body
-                msg.mark_as_read()
+class FlairBot:
 
-if __name__ == '__main__':
-    main()
+    # User agent that PRAW uses to identify your bot to reddit
+    USER_AGENT = '/u/some_user for /r/some_subreddit'
+
+    # User name/password of the account that the bot will run through
+    USER_NAME = 'some_user'
+    PASSWD = 'some_password'
+
+    """ The SUBJECT will be the default subject of your PMs
+    when you create the URLs, eg.
+
+    reddit.com/message/compose/?to=some_user&subject=flair&message=some_message
+
+    PMs require a subject, but it's also a simple way of identifying
+    PMs that are directed towards the flairs and not just a general PM"""
+    SUBJECT = 'flair'
+
+    # TARGET_SUB is the name of the subreddit without the leading /r/
+    TARGET_SUB = 'some_subreddit'
+
+    # Turn on output to log file in current directory - log.txt
+    LOGGING = True
+
+    # Class variable to hold the PRAW instance
+    r = None
+
+    # Class variable to hold the unread pms
+    pms = None
+
+    def login(self):
+        """ Log in to the account being used for the bot """
+        try:
+            self.r = praw.Reddit(user_agent=self.USER_AGENT)
+            self.r.login(self.USER_NAME, self.PASSWD)
+            self.fetch_pms()
+        except:
+            raise
+
+    def fetch_pms(self):
+        """ Get a listing of all unread PMs for the user account """
+        self.pms = self.r.get_unread(limit=None)
+        if self.pms is not None:
+            self.process_pms()
+
+    def process_pms(self):
+        for pm in self.pms:
+            if str(pm.subject) == self.SUBJECT:
+                author = str(pm.author)  # Author of the PM
+                content = str(pm.body)  # Content of the PM
+                subreddit = self.r.get_subreddit(self.TARGET_SUB)
+                if content in flairs:
+                    # Get the flair text that corresponds with the class name
+                    flair_text = str(flairs[content])
+                    subreddit.set_flair(author, flair_text, content)
+                    if self.LOGGING:
+                        self.log(author, content, flair_text)
+                pm.mark_as_read()  # Mark processed PM as read
+
+    def log(self, author, content, flair_text):
+        with open('log.txt', 'a') as logfile:
+            time_now = strftime("%Y-%m-%d %H:%M:%S", gmtime())
+            log_text = 'Added: ' + author + ' : ' \
+                        + flair_text + ' : ' \
+                        + content + ' @ ' + time_now
+            logfile.write(log_text)
+
+FlairBot().login()
