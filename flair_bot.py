add7f16dfcf383a8f90adfbe05cb6b1dd4833675
Jon Berry <jon@jonberry.co>
2017-03-11 19:20:46 -0500

    Resolves a TypeError warning regarding encoding when opening a logfile.

--- flair_bot.py
+++ flair_bot.py
@@ -1,6 +1,7 @@
 """ Flair bot. """
 import sys
 import os
+import io
 import csv
 from configparser import ConfigParser
 from time import gmtime, strftime
@@ -105,7 +106,7 @@ class FlairBot:
     def log(user, text, cls):
         """ Log applied flairs to file. """
 
-        with open('log.txt', 'a', encoding='utf-8') as logfile:
+        with io.open('log.txt', 'a', encoding='utf-8') as logfile:
             time_now = strftime("%Y-%m-%d %H:%M:%S", gmtime())
             log = 'user: ' + user
             log += ' | class(es): ' + cls
